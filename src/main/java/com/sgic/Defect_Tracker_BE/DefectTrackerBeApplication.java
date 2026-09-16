99c9b9dff31988330e8fb8a90fa676ed34d6cb95
dhanu <thanu0281@gamil.com>
2026-09-16 11:20:25 +0530

    first commit

---
+++ src/main/java/com/sgic/Defect_Tracker_BE/DefectTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.sgic.Defect_Tracker_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefectTrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgic/Defect_Tracker_BE/DefectTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgic.Defect_Tracker_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefectTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
